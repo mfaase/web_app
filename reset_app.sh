9acc1618d6804be7389596f8300e39a7ce1194d3
mfaase <miguelfaase@gmail.com>
2024-07-02 19:29:12 +0200

    reset update

--- reset_app.sh
+++ reset_app.sh
@@ -4,8 +4,8 @@ rm -f instance/site.db
 rm -f uploads/*
 rm -rf __pycache__
 pip install -r requirements.txt
-flask db init
-flask db migrate -m "Initial migration"
-flask db upgrade
+# flask db init
+# flask db migrate -m "Initial migration"
+# flask db upgrade
 python add_initial_data.py
 echo "Reset completed."
